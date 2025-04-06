dca748137420143b6486d48ec42eb51c6783832f
sindushekar <shashank.indushekar@dharbor.com>
2025-04-06 12:02:51 +0530

    New Controller Created

--- microservice/src/main/java/com/spring/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/spring/microservice/MicroserviceApplication.java
@@ -2,6 +2,8 @@ package com.spring.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class MicroserviceApplication {
@@ -10,4 +12,8 @@ public class MicroserviceApplication {
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
